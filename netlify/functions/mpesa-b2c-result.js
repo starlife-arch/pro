cab17ccf61877b35c28c459ba6a2f4df18db28b0
starlifeadvert <starlifeadvert@gmail.com>
2026-04-27 21:07:17 +0300

    Update mpesa-b2c-result.js

--- netlify/functions/mpesa-b2c-result.js
+++ netlify/functions/mpesa-b2c-result.js
@@ -1,12 +1,16 @@
+const admin = require('firebase-admin');
+
+if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+}
+const db = admin.firestore();
+
 exports.handler = async (event) => {
   const result = JSON.parse(event.body);
   const { ConversationID, ResultCode, ResultDesc } = result;
-
-  const admin = require('firebase-admin');
-  if (!admin.apps.length) {
-    admin.initializeApp({ credential: admin.credential.applicationDefault() });
-  }
-  const db = admin.firestore();
 
   const wdQuery = await db.collection('withdrawals').where('mpesaConversationId', '==', ConversationID).limit(1).get();
   if (wdQuery.empty) {
